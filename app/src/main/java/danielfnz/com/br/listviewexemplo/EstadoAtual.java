e18bf3e3bc35712ceb901b3f4f243382e1538324
Daniel Leonardo <daniel.fnz@hotmail.com>
2019-02-02 09:16:02 -0200

    Criando classes do projeto

---
+++ app/src/main/java/danielfnz/com/br/listviewexemplo/EstadoAtual.java
@@ -0,0 +1,20 @@
+package danielfnz.com.br.listviewexemplo;
+
+/**
+ * Created by Alunoinf_2 on 02/02/2019.
+ */
+
+public enum EstadoAtual {
+
+    FAZENDO{
+        @Override
+        public String toString() {
+            return "Fazendo";
+        }
+    }, FINALIZADO{
+        @Override
+        public String toString() {
+            return "finalizado";
+        }
+    };
+}
